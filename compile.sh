c80cf46a4c66b3b17f04c78d8271c51f4ecd7f01
grigoryoskin <oskin@phystech.edu>
2021-07-11 18:11:34 +0900

    initial commit

---
+++ compile.sh
@@ -0,0 +1,3 @@
+glslc ../resources/shaders/source/post-process-shader.vert -o ../resources/shaders/generated/post-process-vert.spv
+glslc ../resources/shaders/source/post-process-shader.frag -o ../resources/shaders/generated/post-process-frag.spv
+glslc ../resources/shaders/source/mandelbrot.comp -o ../resources/shaders/generated/mandelbrot.spv
